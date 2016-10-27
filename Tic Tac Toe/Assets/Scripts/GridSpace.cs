842eb1ff6fddd27cea1f8afdfe84dd768b93d7a0
usmansheik <12105089@gift.edu.pk>
2016-10-27 19:05:34 +0500

    Tic Tac Toe Game Complete

---
+++ Tic Tac Toe/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+
+public class GridSpace : MonoBehaviour {
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+
+    private GameController gameController;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+}
